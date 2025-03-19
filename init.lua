1c5c1dd55589beac8587f086eb6fbfd45be388c4
Bo Vial <bvial@macquariecloudservices.com>
2025-03-20 10:41:09 +1100

    add format enable/disable

--- init.lua
+++ init.lua
@@ -770,6 +770,10 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -1074,7 +1078,24 @@ require('lazy').setup({
   },
 })
 
--- Added temporarily to list available snippets
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
 local list_snips = function()
   local ft_list = require('luasnip').available()[vim.o.filetype]
   local ft_snips = {}
